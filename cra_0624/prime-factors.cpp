dd632b605f2f4d481d488ea0b5b0873ca4d30cd6
minho.kim <mh2022.kim@samsung.com>
2024-07-03 11:26:45 +0900

    page 59 add refactoring

--- cra_0624/prime-factors.cpp
+++ cra_0624/prime-factors.cpp
@@ -6,9 +6,8 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		int divisor = 2;
 		if (number > 1) {
-			for (divisor = 2; number > 1; divisor++) {
+			for (int divisor = 2; number > 1; divisor++) {
 				while (number % divisor == 0) {
 					result.push_back(divisor);
 					number /= divisor;
