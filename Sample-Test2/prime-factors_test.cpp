72f5645cdee6aba1ff42866c06b2852c96d75606
minho.kim <mh2022.kim@samsung.com>
2024-07-03 10:39:13 +0900

    first commit

---
+++ Sample-Test2/prime-factors_test.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "../cra_0624/prime-factors.cpp"
+
+class TestFixture : public ::testing::Test {
+public:
+
+private:
+	PrimeFactor prime;
+};
+
+TEST_F(TestFixture, PrimeTest) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(PrimeTest, Test) {
+	EXPECT_EQ(1, 1);
+}
