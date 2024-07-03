8d67c7cc9fbbc1692dc7ef2f94df35706eb632ef
minho.kim <mh2022.kim@samsung.com>
2024-07-03 11:10:46 +0900

    page 44 add test

--- cra_0624/prime-factors.cpp
+++ cra_0624/prime-factors.cpp
@@ -9,6 +9,10 @@ public:
 		if (number > 1 && number < 4) {
 			result.push_back(number);
 		}
+		else if (number == 4) {
+			result.push_back(2);
+			result.push_back(2);
+		}
 		return result;
 	}
 
