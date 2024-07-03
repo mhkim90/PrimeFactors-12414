9d3d08b3faa0925cd35609ef0a99e7706b928da4
minho.kim <mh2022.kim@samsung.com>
2024-07-03 11:13:13 +0900

    page 46 add refactoring

--- cra_0624/prime-factors.cpp
+++ cra_0624/prime-factors.cpp
@@ -6,12 +6,14 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1 && number < 4) {
-			result.push_back(number);
+		if (number > 3) {
+			while (number % 2 == 0) {
+				result.push_back(2);
+				number /= 2;
+			}
 		}
-		else if (number == 4) {
-			result.push_back(2);
-			result.push_back(2);
+		else if (number > 1) {
+			result.push_back(number);
 		}
 		return result;
 	}
